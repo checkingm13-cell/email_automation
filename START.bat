8391ce96ab42da0235dee89332304f1b8f7d742c
checkingm13-cell <checkingm13@gmail.com>
2026-09-04 17:22:03 +0530

    fix: move client script globals to top and disable console quickedit in START.bat

--- START.bat
+++ START.bat
@@ -1,6 +1,10 @@
 @echo off
 title Gmail Mail Merge - Local Control Center
 cd /d "%~dp0"
+
+:: Disable QuickEdit mode so console mouse clicks do not freeze Node.js
+powershell -NoProfile -Command "$t = Add-Type -MemberDefinition '[DllImport(\"kernel32.dll\")] public static extern IntPtr GetStdHandle(int n); [DllImport(\"kernel32.dll\")] public static extern bool GetConsoleMode(IntPtr h, out uint m); [DllImport(\"kernel32.dll\")] public static extern bool SetConsoleMode(IntPtr h, uint m);' -Name 'CK' -Namespace 'W32' -PassThru; $h = $t::GetStdHandle(-10); $m = 0; $t::GetConsoleMode($h, [ref]$m); $t::SetConsoleMode($h, $m -band -bnot 0x0040 -band -bnot 0x0020)" >nul 2>nul
+
 echo ======================================================
 echo Starting Gmail Native Mail Merge Local Control Center...
 echo Dashboard: http://localhost:3000
